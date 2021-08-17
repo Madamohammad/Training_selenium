abe46bc813163f57fafb63d6f0e2d1e08408cdc3
923388 <923388@LTSA000414.cts.com>
2021-08-17 15:56:14 +0300

    project is created

---
+++ JenkinsMavenEx/src/test/java/demo1/GitDemo.java
@@ -0,0 +1,21 @@
+package demo1;
+
+public class GitDemo {
+int []a;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+	
+	public void nn2(){
+		System.out.println("nn2");
+	}
+	public void nn3(){
+		System.out.println("nn3");
+	}
+	public void nn(){
+		System.out.println("nn");
+	}
+
+}
