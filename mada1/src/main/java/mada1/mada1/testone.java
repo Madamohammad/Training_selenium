4802e700b26b771ae0b6581f87ab80e762d58427
923388 <923388@LTSA000414.cts.com>
2021-08-21 21:12:01 +0300

    dd

--- mada1/src/main/java/mada1/mada1/testone.java
+++ mada1/src/main/java/mada1/mada1/testone.java
@@ -3,18 +3,15 @@ package mada1.mada1;
 public class testone {
 	
 	
-<<<<<<< HEAD
+
 	public void t1(){
 		System.out.println("t1");
 	}
-	
-=======
-	
-	
+
 	
 	
 	public void t2(){
 		System.out.println("t2");
 	}
->>>>>>> refs/heads/second
+
 }
